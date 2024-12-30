95e32b7440baaee870b4fe25df0bc71f3e4f9b33
Abhishek Nayak <abhishek.nayak7766@gmail.com>
2024-12-30 14:52:52 +0530

    header

---
+++ src/app/about/page.jsx
@@ -0,0 +1,17 @@
+// import Layout from "../components/CommonLayout";
+import bgImg from "../../../public/images/image.jpg";
+import RootLayout from "../layout";
+import Header from "../components/Header";
+// import RootLayout from "../layout";
+// import RootLayout from "../layout";
+
+const page = () => {
+  return (
+    <div>
+      <Header title="Abot Page" bgImg={bgImg}></Header>
+      about page
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
+import bgImg from "../../public/images/home-page-bg.jpg";
+
+import Header from "./components/Header";
+
+// import RootLayout from "./layout";
 const page = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <section
+        // className=" bg-red-300"
+        style={{
+          backgroundImage: `url(${bgImg.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "100vh",
+        }}
+      >
+        <Header home={true} title="home"></Header>
+      </section>
+      Home
+    </div>
+  );
 };
 
 export default page;
