da4dc2ba23511d270850de1e27ce88b8dd644826
Abhishek Nayak <abhishek.nayak7766@gmail.com>
2025-01-07 13:44:24 +0530

    home

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,12 @@ import Header from "./components/Header";
 import projectImg1 from "../../public/images/home/projectImg2.jpg";
 import Image from "next/image";
 import PlayBtn from "../../public/icons/playBtn";
+import ImageGallery from "./components/ImageGallery";
+import tiles1 from "../../public/images/tiles/tiles1.jpg";
+import tiles2 from "../../public/images/tiles/tiles2.jpg";
+import tiles3 from "../../public/images/tiles/tiles3.jpg";
+import years100 from "../../public/images/home/100years.jpg";
+import RightArrow from "../../public/icons/RightArrow";
 const page = () => {
   const project = {
     year: "100 years of",
@@ -10,8 +16,15 @@ const page = () => {
     featured: "Featured in Vogue magazine, Feb 2023",
     location: "LUXOTEL, Art Gallery Goa",
     designBy: "Designed by Xyz, Firdaus Designer",
-    tilesImg: [],
+    tilesImg: [tiles1, tiles2, tiles3],
   };
+
+  const imgArray = [
+    "https://images.unsplash.com/photo-1602023039928-7af5a2f000fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1726221618316-ea7b80ca135e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1733259782090-d33273213e83?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+
   return (
     <div>
       <section
@@ -34,7 +47,7 @@ const page = () => {
             </p>
           </div>
           <div className=" normal-text mb-6  lg:mb-16  flex w-full">
-            <div className=" text-[12px]   border-red-400 md:text-base  w-full">
+            <div className="  border-red-400 md:text-base text-[12px]    w-full">
               <p>{project.location}</p>
               <p>{project.designBy}</p>
             </div>
@@ -104,6 +117,80 @@ const page = () => {
               </p>
             </div>
           </div>
+        </div>
+      </section>
+
+      <ImageGallery images={imgArray} />
+      <section className="relative font-Inter pt-9 pb-10">
+        <div className="absolute hidden lg:flex w-full h-full px-[4px] lg:px-[88px]  top-0 left-0  justify-between">
+          <div className=" w-full border-l border-[#2A2523] opacity-[0.1] h-full"></div>
+          <div className=" w-full border border-[#2A2523] opacity-[0.1] h-full"></div>
+          <div className=" w-full border-r border-[#2A2523] opacity-[0.1] h-full"></div>
+        </div>
+        <div className=" md:text-base text-[12px]  md:pt-14  flex justify-between items-end md:items-start side-padding">
+          <div className="  w-full text-start">
+            <p>{project?.location}</p>
+            <p>{project?.designBy}</p>
+            <div className=" flex gap-4 mt-5">
+              {project?.tilesImg.map((img, i) => (
+                <Image alt="img" src={img} key={i}></Image>
+              ))}
+            </div>
+          </div>
+          <p className="  w-full text-center hidden lg:block">
+            {project?.featured}
+          </p>
+          <a className="  w-[150px] mb-4  md:w-full text-end" href="">
+            View Projects
+          </a>
+        </div>
+        <div className=" mt-[105px] lg:mt-56  side-padding ">
+          <p className=" leading-10  font-Gloock text-[2.1rem] lg:text-[100px] lg:leading-[100px] text-start  md:text-center text-[#304941]">
+            We blend computer-aided design with craftsmanship and the end result
+            is a tailor-made custom tile which will last for generations.
+            Already 100 years and counting...
+          </p>
+          <Image
+            alt="img"
+            className=" mx-auto w-40 mt-16 lg:mt-[72px]"
+            src={years100}
+          ></Image>
+          <div className=" hidden lg:flex justify-between mt-[135px]">
+            <p>4899 projects</p>
+            <p>+3 million tiles placed</p>
+            <p>100 years, 7 months, 6 days, 12 hours and 13 minutes</p>
+          </div>
+        </div>
+      </section>
+
+      <ImageGallery images={imgArray} />
+      <section className="relative font-Inter pt-9 md:pt-0 pb-24 md:pb-[199px]">
+        <div className="absolute hidden lg:flex w-full h-full px-[4px] lg:px-[88px]  top-0 left-0  justify-between">
+          <div className=" w-full border-l border-[#2A2523] opacity-[0.1] h-full"></div>
+          <div className=" w-full border border-[#2A2523] opacity-[0.1] h-full"></div>
+          <div className=" w-full border-r border-[#2A2523] opacity-[0.1] h-full"></div>
+        </div>
+        <div className=" md:text-base text-[12px]  md:pt-14  flex justify-between items-end md:items-start side-padding">
+          <div className="  w-full text-start">
+            <p>{project?.location}</p>
+            <p>{project?.designBy}</p>
+            <div className=" flex gap-4 mt-5">
+              {project?.tilesImg.map((img, i) => (
+                <Image alt="img" src={img} key={i}></Image>
+              ))}
+            </div>
+          </div>
+          <p className="  w-full text-center hidden lg:block">
+            {project?.featured}
+          </p>
+          <p className="  w-[260px] mb-4 flex   md:items-start justify-end  md:w-full">
+            <a
+              className=" md:border  justify-center items-center flex gap-3 border-black text-[14px] md:text-base  md:w-fit md:px-5 md:py-[15px] "
+              href="#"
+            >
+              View Projects <RightArrow />
+            </a>
+          </p>
         </div>
       </section>
     </div>
